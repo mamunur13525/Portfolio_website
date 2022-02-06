7c6e17e6b4a5c220652bae03f4b255fbec130836
Mamun React W3Dev <mamun@w3dev.email>
2022-02-07 01:33:17 +0600

    done

--- src/components/FakeData.js
+++ src/components/FakeData.js
@@ -4,6 +4,7 @@ import voleenterThumbnail from '../image/works/voleenter_works_thumbnail.jpg';
 import voleenterFullPhoto from '../image/works/voleenter_work_full.png';
 import netflixClone from '../image/works/netflix-clone.png';
 import netflixLandingPage from '../image/works/netflix-landing-page.png';
+import freeCourse from '../image/works/free-course.png';
 
 export const fakeData = [
   {
@@ -71,9 +72,9 @@ export const fakeData = [
   {
     title: "Course Blog Site",
     sub_title: "This is sub title for the createive website.",
-    live_link: "https://creative-agency-8d8f6.web.app/",
-    github_Link: [{ title: 'Frontend Code', url: "https://github.com/mamunur13525/free_course_front" }, { title: 'Backend Code', url: 'https://github.com/mamunur13525/FreeCourseBackend.git' }],
-    thumbail_image: creative_thumbnail,
+    live_link: "https://course-free.netlify.app",
+    github_Link: [{ title: 'Frontend Code', url: "https://github.com/mamunur13525/free_course_front" }, { title: 'Backend Code', url: 'https://github.com/mamunur13525/FreeCourseBackend' }],
+    thumbail_image: freeCourse,
     full_photo: creativeAgency,
     images: "https://source.unsplash.com/aJeH0KcFkuc/400x300",
     category: 'React',
@@ -130,89 +131,5 @@ export const fakeData = [
     ],
     details:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo dicta a reiciendis quisquam blanditiis voluptate culpa sint saepe doloribus? Sint repudiandae enim praesentium dignissimos veniam soluta laboriosam temporibus nobis explicabo.",
-  },
-  {
-    title: "Creative Agency",
-    sub_title: "This is sub title for the createive website.",
-    live_link: "https://creative-agency-8d8f6.web.app/",
-    github_Link: [{ title: 'Frontend Code', url: "https://github.com/mamunur13525/Creative_Agency" }],
-    thumbail_image: creative_thumbnail,
-    full_photo: creativeAgency,
-    images: "https://source.unsplash.com/aJeH0KcFkuc/400x300",
-    category: 'React',
-    technology_used: [
-      "Javascript",
-      "React",
-      "MongoDB",
-      "Node.js",
-      "Express",
-      "HTML5",
-      "CSS3",
-    ],
-    details:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo dicta a reiciendis quisquam blanditiis voluptate culpa sint saepe doloribus? Sint repudiandae enim praesentium dignissimos veniam soluta laboriosam temporibus nobis explicabo.",
-  },
-  {
-    title: "Creative Agency",
-    sub_title: "This is sub title for the createive website.",
-    live_link: "https://creative-agency-8d8f6.web.app/",
-    github_Link: [{ title: 'Frontend Code', url: "https://github.com/mamunur13525/Creative_Agency" }],
-    thumbail_image: creative_thumbnail,
-    full_photo: creativeAgency,
-    images: "https://source.unsplash.com/aJeH0KcFkuc/400x300",
-    category: 'React',
-    technology_used: [
-      "Javascript",
-      "React",
-      "MongoDB",
-      "Node.js",
-      "Express",
-      "HTML5",
-      "CSS3",
-    ],
-    details:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo dicta a reiciendis quisquam blanditiis voluptate culpa sint saepe doloribus? Sint repudiandae enim praesentium dignissimos veniam soluta laboriosam temporibus nobis explicabo.",
-  },
-  {
-    title: "Creative Agency",
-    sub_title: "This is sub title for the createive website.",
-    live_link: "https://creative-agency-8d8f6.web.app/",
-    github_Link: [{ title: 'Frontend Code', url: "https://github.com/mamunur13525/Creative_Agency" }],
-    thumbail_image: creative_thumbnail,
-    full_photo: creativeAgency,
-    images: "https://source.unsplash.com/aJeH0KcFkuc/400x300",
-    category: 'React',
-    technology_used: [
-      "Javascript",
-      "React",
-      "MongoDB",
-      "Node.js",
-      "Express",
-      "HTML5",
-      "CSS3",
-    ],
-    details:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo dicta a reiciendis quisquam blanditiis voluptate culpa sint saepe doloribus? Sint repudiandae enim praesentium dignissimos veniam soluta laboriosam temporibus nobis explicabo.",
-  },
-  {
-    title: "Creative Agency",
-    sub_title: "This is sub title for the createive website.",
-    live_link: "https://creative-agency-8d8f6.web.app/",
-    github_Link: [{ title: 'Frontend Code', url: "https://github.com/mamunur13525/Creative_Agency" }],
-    thumbail_image: creative_thumbnail,
-    full_photo: creativeAgency,
-    images: "https://source.unsplash.com/aJeH0KcFkuc/400x300",
-    category: 'React',
-    technology_used: [
-      "Javascript",
-      "React",
-      "MongoDB",
-      "Node.js",
-      "Express",
-      "HTML5",
-      "CSS3",
-    ],
-    details:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo dicta a reiciendis quisquam blanditiis voluptate culpa sint saepe doloribus? Sint repudiandae enim praesentium dignissimos veniam soluta laboriosam temporibus nobis explicabo.",
-  },
+  }
 ];
